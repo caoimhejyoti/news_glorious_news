b44abf1630b7241ea4834a076ae01ff7f5b02696
caoimhejyoti <caoimhejyoti@gmail.com>
2023-11-25 12:59:55 +0800

    add story view

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -15,3 +15,9 @@ class IndexView(generic.ListView):
         context = super().get_context_data(**kwargs)
         context['latest_stories'] = NewsStory.objects.all()[:4]
         return context
+
+
+class StoryView(generic.DetailView):
+    model = NewsStory
+    template_name = 'new/story.html'
+    context_object_name = 'story'
