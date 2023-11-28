37afa8285c2901c72c2ac8e45b23ba4c8d49090b
caoimhejyoti <caoimhejyoti@gmail.com>
2023-11-28 19:40:47 +0800

    creating CustomUser model

--- she_codes_news/users/models.py
+++ she_codes_news/users/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    pass 
+
+    def __str__(self): #reserved python method - if there is an object, we are dictating how to display the string version of the user.
+        return self.username
