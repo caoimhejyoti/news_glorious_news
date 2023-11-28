56f10384089eb2a5fb2eb04661ade39569fa67fe
caoimhejyoti <caoimhejyoti@gmail.com>
2023-11-28 20:06:45 +0800

    reordering installed apps

--- she_codes_news/she_codes_news/settings.py
+++ she_codes_news/she_codes_news/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.UsersConfig'
     'news.apps.NewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig'
 ]
 
 AUTH_USER_MODEL = 'users.CustomUser'
