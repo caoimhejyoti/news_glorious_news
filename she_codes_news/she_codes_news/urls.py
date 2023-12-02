78f874e9e388c3c1fdd4be8dcf9049d1f6f703eb
caoimhejyoti <caoimhejyoti@gmail.com>
2023-12-02 08:04:34 +0800

    tidyi ng up

--- she_codes_news/she_codes_news/urls.py
+++ she_codes_news/she_codes_news/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('news/', include('news.urls')),
     path('admin/', admin.site.urls),
-    path('users/', include('django.contrib.auth.urls'))
-    
+    path('users/', include('django.contrib.auth.urls')),
 ]
