85c6313fddab4706f43022ddfc2a6305dfe5c4ce
caoimhejyoti <caoimhejyoti@gmail.com>
2023-12-05 20:10:18 +0800

    getting authored stories to profile page - BUGGY

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db import models
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -6,14 +8,26 @@ from django.views import generic
 from .models import CustomUser
 from .forms import CustomUserCreationForm
 
+from news.models import NewsStory
+
+
 class CreateAccountView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'users/createAccount.html'
 
-class UserProfileView(LoginRequiredMixin, CreateView):
+class UserProfileView(LoginRequiredMixin, generic.DetailView):
     login_url = "login"
     success_url = reverse_lazy('users:profile') #on form success, take user back to profile.
     model = CustomUser
     template_name = "users/userProfile.html"
     form_class = CustomUserCreationForm
+
+    # def get_queryset(self):
+    #     return NewsStory.objects.all()
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['authored_stories'] = NewsStory.objects.filter(author=self.kwargs['pk']) 
+        return context
+        
